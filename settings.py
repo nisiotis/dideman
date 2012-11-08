d0ec1c965c6a30d25ac2fdea9e601adf7183a770
Stathis Stergou <sstergou@gmail.com>
2012-11-08 14:12:50 +0200

    fixed settings bug

--- settings.py
+++ settings.py
@@ -130,7 +130,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     #'django.contrib.admindocs',
     'dideman.dide',
-    'admin_tabs',
     'south',
     #'debug_toolbar',
 )
