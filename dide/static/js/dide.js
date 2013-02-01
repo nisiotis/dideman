6d16864e7951dca7936fba4c5b3d5b2ddd5dbe60
Stathis Stergou <sstergou@gmail.com>
2013-02-01 11:51:56 +0200

    updated dide/static/js/dide.js

--- dide/static/js/dide.js
+++ dide/static/js/dide.js
@@ -1,19 +1,21 @@
 (function($) {
     $(document).ready(function() {
         var documentWidth = $(document).width();
-        oldFunc = DateTimeShortcuts.openCalendar;
-        DateTimeShortcuts.openCalendar = function(num) {
-            oldFunc.call(DateTimeShortcuts, num);
-            var calBox = $('#calendarbox'+num).get(0);
-            var calBoxWidth = $(calBox).width();
-            if(calBoxWidth + parseInt(calBox.style.left) > documentWidth) {
-                calBox.style.left = documentWidth - calBoxWidth - 5 + 'px';
+        if(window.hasOwnProperty('DateTimeShortcuts')) {
+            oldFunc = DateTimeShortcuts.openCalendar;
+            DateTimeShortcuts.openCalendar = function(num) {
+                oldFunc.call(DateTimeShortcuts, num);
+                var calBox = $('#calendarbox'+num).get(0);
+                var calBoxWidth = $(calBox).width();
+                if(calBoxWidth + parseInt(calBox.style.left) > documentWidth) {
+                    calBox.style.left = documentWidth - calBoxWidth - 5 + 'px';
+                }
+                calBox.style.zIndex = 1001;
             }
-            calBox.style.zIndex = 1001;
         }
-    });
+    });    
+    
 })(django.jQuery)
-
 
 function focusOrOpen(link, name) {
     var popup = window.open('', name, 'height=500,width=800,resizable=yes,scrollbars=yes');
