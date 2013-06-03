c66427fd9948b0d167c4c763e973d41d690b1519
Stathis Stergou <sstergou@gmail.com>
2013-06-03 14:22:50 +0300

    fixed url in employeeleave

--- dide/templatetags/custom_admin.py
+++ dide/templatetags/custom_admin.py
@@ -40,7 +40,7 @@ def submit_row(context):
         'onclick_attrib': (opts.get_ordered_objects() and change
                             and 'onclick="submitOrderForm();"' or ''),
         'show_delete_link': (not is_popup and context['has_delete_permission']
-                              and (change or context['show_delete'])),
+                              and (change or context.get('show_delete', True))),
         'show_save_as_new': not is_popup and change and save_as,
         'show_save_and_add_another': context['has_add_permission'] and
                             not is_popup and (not save_as or context['add']),
