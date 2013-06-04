dbb821b76a43f390df508c6d338d42aba577356a
Stathis Stergou <sstergou@gmail.com>
2013-06-04 08:45:29 +0300

    fixed global static setting

--- settings.py
+++ settings.py
@@ -86,7 +86,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.realpath(os.path.join(os.path.dirname(__file__), 'global_static')),
+    os.path.realpath(os.path.join(os.path.dirname(__file__), 'global')),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
