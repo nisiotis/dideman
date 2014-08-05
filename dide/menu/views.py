7c8733a5ac734188a70e685f2aeb9c9f2e533f80
Stathis Stergou <sstergou@gmail.com>
2014-08-05 12:54:33 +0300

    Applications shown on menu minor fix on dates

--- dide/menu/views.py
+++ dide/menu/views.py
@@ -13,7 +13,7 @@ _template_path = 'menu' + os.path.sep
 
 def menu(request):
     today = datetime.date.today()
-    set = ApplicationSet.objects.filter(end_date__gte=today)
+    set = ApplicationSet.objects.filter(end_date__gte=today, start_date__lte=today)
     if 'logout' in request.GET:
         request.session.clear()
         messages.info(request, 'Αποσυνδεθήκατε με επιτυχία. Σε περίπτωση που χρησιμοποιήσατε έναν δημόσιο ηλεκτρονικό υπολογιστή, παρακαλούμε κλείστε το πρόγραμμα περιήγησης (browser).')
