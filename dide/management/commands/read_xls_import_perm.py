722d332ce3030bbec7062091047fa5b9e847d5ab
Vasilios Kasapidis <kassapidis@gmail.com>
2022-09-20 12:26:49 +0300

    Last Changes

--- dide/management/commands/read_xls_import_perm.py
+++ dide/management/commands/read_xls_import_perm.py
@@ -19,19 +19,20 @@ class Command(BaseCommand):
             worksheet = workbook.sheet_by_index(0)
             curr_row = 0
             while curr_row < worksheet.nrows:
-                p = Permanent(vat_number=unicode(worksheet.cell_value(curr_row,0)), 
+                p = Permanent(registration_number=unicode(worksheet.cell_value(curr_row,0))[:6], 
                             lastname=unicode(worksheet.cell_value(curr_row,1)),
                             firstname=unicode(worksheet.cell_value(curr_row,2)),
                             fathername=unicode(worksheet.cell_value(curr_row,3)),
                             profession=Profession.objects.get(pk=unicode(worksheet.cell_value(curr_row,4))),
                             transfer_area=TransferArea.objects.get(pk=int(worksheet.cell_value(curr_row,5))),
-                            telephone_number1=int(worksheet.cell_value(curr_row,6)),
-                            telephone_number2=int(worksheet.cell_value(curr_row,7)),
-                            email=unicode(worksheet.cell_value(curr_row,8)),
-                            date_hired=unicode(worksheet.cell_value(curr_row,9)),
-                            order_hired=unicode(worksheet.cell_value(curr_row,10)))
+                            #telephone_number1=int(worksheet.cell_value(curr_row,6)),
+                            #telephone_number2=int(worksheet.cell_value(curr_row,7)),
+                            #email=unicode(worksheet.cell_value(curr_row,8)),
+                            #date_hired=unicode(worksheet.cell_value(curr_row,9)),
+                            order_hired=unicode(worksheet.cell_value(curr_row,6)))
                 print p
                 try:
+                    
                     p.save()
                 except Exception as ex:
                     print(ex)
