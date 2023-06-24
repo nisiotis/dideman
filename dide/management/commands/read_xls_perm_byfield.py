ea4ced0e000090e09a265d06ebdf343c13b9f8e9
nisiotis <kassapidis@gmail.com>
2023-06-25 00:26:17 +0300

    Commands update 2

--- dide/management/commands/read_xls_perm_byfield.py
+++ dide/management/commands/read_xls_perm_byfield.py
@@ -20,7 +20,8 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option('--f', type=str, help='The xls file'),
         make_option('--ci', type=int, help='The column field'),
-        make_option('--df', type=str, help='The field')
+        make_option('--df', type=str, help='The field'),
+        make_option('--ws', type=int, help='The sheet index of xls book')
     )
     
     help = 'XLS database import.'
@@ -28,7 +29,7 @@ class Command(BaseCommand):
         field_name = ""
         for f in model._meta.fields:
             if f.name == df:
-                field_name = f.name
+                field_name = f
         return field_name
 
     def handle(self, *args, **options):
@@ -40,13 +41,17 @@ class Command(BaseCommand):
                 exit()
             if options['df'] != '':
                 if self.find_model_field(Permanent, options['df']) != "":
-                    worksheet = workbook.sheet_by_index(0)
+                    worksheet = workbook.sheet_by_index(options['ws']) if options['ws'] else workbook.sheet_by_index(0)
                     curr_row = 0
                     idx = options['ci'] if options['ci'] else 1
                     try:
+                        print "Field to inport: %s" % self.find_model_field(Permanent, options['df']).name
                         while curr_row < worksheet.nrows:
-                            print worksheet.cell_value(curr_row,0), worksheet.cell_value(curr_row,idx)
+                            cell = worksheet.cell(curr_row,idx)
+                            
+                            print worksheet.cell_value(curr_row,0), cell.value, cell.ctype
                             curr_row += 1
+                        print self.find_model_field(Permanent, options['df']).get_internal_type()
                     except:
                         print "Error in reading xls file"
                         exit()
